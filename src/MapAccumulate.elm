b97ceea8f1b59ceea0014819becb7ab446c99e43
Robert K. Bell <robert.k.bell@gmail.com>
2017-12-10 20:14:26 +1100

    feat: add more examples, tests; clean up for packaging

--- src/MapAccumulate.elm
+++ src/MapAccumulate.elm
@@ -1,24 +1,119 @@
 module MapAccumulate exposing (mapAccumL)
 
-{-| dfoo
+{-|
+
+
+# MapAccumulate
+
 @docs mapAccumL
+
 -}
 
 
-{-| mapAccumL
+{-| From [Haskell's documentation][haskelldocs]:
+
+> The mapAccumL function behaves like a combination of fmap and foldl; it
+> applies a function to each element of a structure, passing an accumulating
+> parameter from left to right, and returning a final value of this
+> accumulator together with the new structure.
+
+Originally based on fatho's [labyrinth] project.
+
+[haskelldocs]: https://hackage.haskell.org/package/base-4.10.1.0/docs/Data-List.html#v:mapAccumL
+[labyrinth]: https://github.com/fatho/labyrinth/blob/8a522c0bdfbd5459bc00d9b1ba3334483995dbd7/Util.elm#L28-L35
+
+
+### example 1
+
+Given a string `a` and a list of integers, test each integer `b`.
+If `b` is odd, append it to `a`, and multiply it by 10 inside the list.
+
+    a = ""
+    b = [ 1, 2, 3, 4 ]
+
+    func : Int -> String -> ( Int, String )
+    func b a =
+        if b % 2 == 0 then
+            -- even
+            ( b, a )
+        else
+            -- odd
+            ( b * 10, a ++ "," ++ toString b )
+
+    mapAccumL func "" [ 1, 2, 3, 4 ]
+        -- == `( [ 10, 2, 30, 4], ",1,3" )`.
+
+
+### example 2
+
+Given a string `a` and a list of integers, test each integer `b`.
+If `b` is odd, append it to `a`, and remove it from the list.
+
+    a = ""
+    b = [ 1, 2, 3, 4 ]
+
+    func : Int -> String -> ( Maybe Int, String )
+    func b a =
+        if b % 2 == 0 then
+            -- even
+            ( Just b, a )
+        else
+            -- odd
+            ( Nothing, a ++ "," ++ toString b )
+
+    justSomethings : (List (Maybe a), b) -> (List a, b)
+    justSomethings =
+        (\( list, b ) -> ( list |> Maybe.Extra.values, b ))
+
+    mapAccumL func "" [ 1, 2, 3, 4 ]
+        |> justSomethings
+        -- == `( [ 2, 4], ",1,3" )`.
+
+
+### example 3
+
+Given a string `a` and a list of integers, test `a` and each integer `b`.
+If `b` is odd, append it to `a`, and remove it from the list.
+If `b` is `42`, the final `a` must equal `towel!`, and no further `b`s
+should be removed from the list.
+
+    a = ""
+    b = [ 1, 2, 3, 4 ]
+
+    func : Int -> String -> ( Maybe Int, String )
+    func b a =
+        if a == "towel!" then
+            ( Just b, a )
+        else if b == 42 then
+            ( Just b, "towel!" )
+        else if b % 2 == 0 then
+            -- even
+            ( Just b, a )
+        else
+            -- odd
+            ( Nothing, a ++ "," ++ toString b )
+
+    justSomethings : (List (Maybe a), b) -> (List a, b)
+    justSomethings =
+        (\( list, b ) -> ( list |> Maybe.Extra.values, b ))
+
+    mapAccumL func "" [ 1, 2, 3, 42, 5, 6, 7 ]
+        |> justSomethings
+        -- == `( [ 2, 42, 5, 6, 7 ], "towel!" )`
+
 -}
 mapAccumL : (a -> acc -> ( b, acc )) -> acc -> List a -> ( List b, acc )
-mapAccumL f acc xxs =
+mapAccumL f acc0 xxs =
     case xxs of
         [] ->
-            ( [], acc )
+            ( [], acc0 )
 
         x :: xs ->
             let
-                ( y, acc_ ) =
-                    f x acc
+                ( y, acc1 ) =
+                    f x acc0
 
-                ( ys, acc__ ) =
-                    mapAccumL f acc_ xs
+                ( ys, acc2 ) =
+                    mapAccumL f acc1 xs
             in
-                ( y :: ys, acc__ )
+                ( y :: ys, acc2 )
--- tests/TestMapAccumulate.elm
+++ tests/TestMapAccumulate.elm
@@ -3,11 +3,12 @@ module TestMapAccumulate exposing (suite)
 import Expect exposing (Expectation, fail)
 import Test exposing (describe, test, Test)
 import MapAccumulate exposing (mapAccumL)
+import Maybe.Extra
 
 
 suite =
     describe "main"
-        [ test "basic example" <|
+        [ test "example 1" <|
             let
                 func : Int -> String -> ( Int, String )
                 func b a =
@@ -21,4 +22,38 @@ suite =
                 \_ ->
                     mapAccumL func "" [ 1, 2, 3, 4 ]
                         |> Expect.equal ( [ 10, 2, 30, 4 ], ",1,3" )
+        , test "example 2 - removal" <|
+            let
+                func : Int -> String -> ( Maybe Int, String )
+                func b a =
+                    if b % 2 == 0 then
+                        -- even
+                        ( Just b, a )
+                    else
+                        -- odd
+                        ( Nothing, a ++ "," ++ toString b )
+            in
+                \_ ->
+                    mapAccumL func "" [ 1, 2, 3, 4 ]
+                        |> (\( list, acc ) -> ( list |> Maybe.Extra.values, acc ))
+                        |> Expect.equal ( [ 2, 4 ], ",1,3" )
+        , test "example 3 - removal with clearing" <|
+            let
+                func : Int -> String -> ( Maybe Int, String )
+                func b a =
+                    if a == "towel!" then
+                        ( Just b, a )
+                    else if b == 42 then
+                        ( Just b, "towel!" )
+                    else if b % 2 == 0 then
+                        -- even
+                        ( Just b, a )
+                    else
+                        -- odd
+                        ( Nothing, a ++ "," ++ toString b )
+            in
+                \_ ->
+                    mapAccumL func "" [ 1, 2, 3, 42, 5, 6, 7 ]
+                        |> (\( list, acc ) -> ( list |> Maybe.Extra.values, acc ))
+                        |> Expect.equal ( [ 2, 42, 5, 6, 7 ], "towel!" )
         ]
