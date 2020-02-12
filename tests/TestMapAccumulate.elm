a98825b3ad4b1cae6620a2ed7ed69f8dda475493
Robert K. Bell <robert.k.bell@gmail.com>
2020-02-12 21:14:47 +1100

    chore: fix up ancient test, use latest version of elm & elm-test

--- tests/TestMapAccumulate.elm
+++ tests/TestMapAccumulate.elm
@@ -1,8 +1,8 @@
 module TestMapAccumulate exposing (suite)
 
-import Expect exposing (Expectation, fail)
-import Test exposing (describe, test, Test)
+import Expect exposing (Expectation)
 import MapAccumulate exposing (mapAccumL)
+import Test exposing (Test, describe, test)
 
 
 suite =
@@ -11,57 +11,62 @@ suite =
             let
                 func : Int -> String -> ( Int, String )
                 func b a =
-                    if b % 2 == 0 then
+                    if (b |> modBy 2) == 0 then
                         -- even
                         ( b, a )
+
                     else
                         -- odd
-                        ( b * 10, a ++ "," ++ toString b )
+                        ( b * 10, a ++ "," ++ (b |> String.fromInt) )
             in
-                \_ ->
-                    mapAccumL func "" [ 1, 2, 3, 4 ]
-                        |> Expect.equal ( [ 10, 2, 30, 4 ], ",1,3" )
+            \_ ->
+                mapAccumL func "" [ 1, 2, 3, 4 ]
+                    |> Expect.equal ( [ 10, 2, 30, 4 ], ",1,3" )
         , test "example 2 - removal" <|
             let
                 func : Int -> String -> ( Maybe Int, String )
                 func b a =
-                    if b % 2 == 0 then
+                    if (b |> modBy 2) == 0 then
                         -- even
                         ( Just b, a )
+
                     else
                         -- odd
-                        ( Nothing, a ++ "," ++ toString b )
+                        ( Nothing, a ++ "," ++ (b |> String.fromInt) )
             in
-                \_ ->
-                    mapAccumL func "" [ 1, 2, 3, 4 ]
-                        |> (\( list, acc ) -> ( list |> values, acc ))
-                        |> Expect.equal ( [ 2, 4 ], ",1,3" )
+            \_ ->
+                mapAccumL func "" [ 1, 2, 3, 4 ]
+                    |> (\( list, acc ) -> ( list |> values, acc ))
+                    |> Expect.equal ( [ 2, 4 ], ",1,3" )
         , test "example 3 - removal with clearing" <|
             let
                 func : Int -> String -> ( Maybe Int, String )
                 func b a =
                     if a == "towel!" then
                         ( Just b, a )
+
                     else if b == 42 then
                         ( Just b, "towel!" )
-                    else if b % 2 == 0 then
+
+                    else if (b |> modBy 2) == 0 then
                         -- even
                         ( Just b, a )
+
                     else
                         -- odd
-                        ( Nothing, a ++ "," ++ toString b )
+                        ( Nothing, a ++ "," ++ (b |> String.fromInt) )
             in
-                \_ ->
-                    mapAccumL func "" [ 1, 2, 3, 42, 5, 6, 7 ]
-                        |> (\( list, acc ) -> ( list |> values, acc ))
-                        |> Expect.equal ( [ 2, 42, 5, 6, 7 ], "towel!" )
+            \_ ->
+                mapAccumL func "" [ 1, 2, 3, 42, 5, 6, 7 ]
+                    |> (\( list, acc ) -> ( list |> values, acc ))
+                    |> Expect.equal ( [ 2, 42, 5, 6, 7 ], "towel!" )
         ]
 
 
 {-| Convert a list of `Maybe a` to a list of `a` only for the values different
 from `Nothing`.
 
-    values [ Just 1, Nothing, Just 2 ] == [1, 2]
+    values [ Just 1, Nothing, Just 2 ] == [ 1, 2 ]
 
 -}
 values : List (Maybe a) -> List a
